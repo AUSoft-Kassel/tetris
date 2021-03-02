f1c94d64324e88b1d05bc2432d7ffe3a3bd94b03
laim93 <78472064+laim93@users.noreply.github.com>
2021-03-02 15:21:35 +0100

    Fixes Issues #3 #17 #18 (#20)
    * Fixes Issues #3 #17 #18
    
    * updates
    
    Co-authored-by: Andreas Hoefer <hoefer74@gmail.com>

---
+++ lib/models/positions.dart
@@ -0,0 +1,12 @@
+/// Description of the class which define our position
+class Position {
+  /// Descriptes in which row position it will be shown
+  final int _x;
+  /// Descriptes in which column position it will be shown
+  final int _y;
+  /// Defines the position of our shapes later
+  Position(int x, int y):_x=x,_y=y;
+}
+
+
+

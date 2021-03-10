4423e679f255f368f36d7465748d5ca1cf1198db
Andreas Hoefer <hoefer74@gmail.com>
2021-03-10 15:41:00 +0100

    testing

--- test/rotation_test.dart
+++ test/rotation_test.dart
@@ -1,12 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tetris/models/gamestatus.dart';
 import 'package:tetris/models/rotation.dart';
 
 void main() {
   test('Rotation can be constructed and read', () {
     const left = Rotation.left;
     const right = Rotation.right;
-    expect(left is GameStatus, true);
-    expect(right is GameStatus, true);
+    expect(left is Rotation, true);
+    expect(right is Rotation, true);
   });
 }

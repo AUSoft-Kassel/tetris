ca0a9c1deaf7abb2f64cec206f4ce96ea8dd207f
Andre <a.heinicke2988@gmail.com>
2021-03-10 14:37:55 +0100

    [CODE] Create enum Direction in /lib/models/direction.dart Fixes #15s

---
+++ lib/models/direktion.dart
@@ -0,0 +1,14 @@
+/// Moving direktion of the shape
+enum Direction {
+  /// Direction up
+  up,
+
+  /// Direction down
+  down,
+
+  /// Direction left
+  left,
+
+  /// Direction right
+  right,
+}

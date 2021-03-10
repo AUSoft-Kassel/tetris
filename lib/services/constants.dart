ff619bb6f226ae244cfdf3009bfbc072b7b8b98a
Andre <a.heinicke2988@gmail.com>
2021-03-10 15:32:08 +0100

    [CODE] More global settings in class Constants Fixes #27

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,10 +1,14 @@
-
-
 /// Class which descripts the static constants of our Screen
 abstract class Constants {
   /// Descriptes the number of Rows
-  static const numRows= 20;
-  /// Descriptes the number of columns
-  static const numCols= 10;
+  static const numRows = 20;
 
+  /// Descriptes the number of columns
+  static const numCols = 10;
+
+  /// Minimum gamespeed
+  static const minSpeed = 10;
+
+  /// Maximum gamespeed
+  static const maxSpeed = 100;
 }

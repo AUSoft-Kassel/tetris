7efde20508047cd061d0e5ad62b403ac551824ed
Andreas Hoefer <hoefer74@gmail.com>
2021-03-10 15:38:37 +0100

    update constants

--- test/constants_test.dart
+++ test/constants_test.dart
@@ -4,8 +4,12 @@ import 'package:tetris/services/constants.dart';
 void main() {
   test('Constants can be read and have the expected value', () {
     expect(Constants.numRows, 20);
+    expect(Constants.numRows is int, true);
     expect(Constants.numCols, 10);
+    expect(Constants.numCols is int, true);
     expect(Constants.minSpeed, 10.0);
+    expect(Constants.minSpeed is double, true);
     expect(Constants.maxSpeed, 100.0);
+    expect(Constants.maxSpeed is double, true);
   });
 }

701ba9908476ab9736e30c8ec51bccbeaa8dcd9d
Andreas Hoefer <hoefer74@gmail.com>
2021-03-10 15:34:24 +0100

    more tests

--- test/shape_z_test.dart
+++ test/shape_z_test.dart
@@ -2,11 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tetris/models/position.dart';
 import 'package:tetris/models/rotation.dart';
 import 'package:tetris/models/shape.dart';
-import 'package:tetris/models/shape_i.dart';
+import 'package:tetris/models/shape_z.dart';
 
 void main() {
-  test('ShapeI can be constructed and is a shape', () {
-    var shape = ShapeI();
+  test('ShapeZ can be constructed and is a shape', () {
+    var shape = ShapeZ();
     expect(shape.getAbsPositions(base: Position(10, 10)) is List<Position>, true);
     expect(shape.getRotatedAbsPositions(base: Position(10, 10), rotation: Rotation.left) is Shape, true);
   });

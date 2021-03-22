8e19250ba9b1f3e73e624b0765eadcfa081e75d6
Andreas Hoefer <hoefer74@gmail.com>
2021-03-22 17:11:36 +0100

    more tests

--- test/shapeshop.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:tetris/models/shapeshop.dart';
-
-void main() {
-  test('ShapeShop can be constructed', () {
-    var shapeShop = ShapeShop();
-    expect(shapeShop is ShapeShop, true);
-  });
-}

489a968297073cc0a3562be67050b2ef247e372e
Frank292 <f.merkel92@hotmail.com>
2021-03-23 15:16:09 +0100

    shapes

--- lib/models/shapes.dart
+++ lib/models/shapes.dart
@@ -11,5 +11,71 @@ class ShapeO extends Shape {
       Position(1, -1),
     ],
   ]; // [0,0],[0,1],[1,0],[1,-1]
-
 }
+
+class ShapeI extends Shape {
+  final List<List<Position>> _shapeStates = [
+    [
+      Position(0, 0),
+      Position(0, 1),
+      Position(1, 0),
+      Position(1, -1),
+    ],
+  ]; // [0,0],[0,1],[1,0],[1,-1]
+}
+
+class ShapeL extends Shape {
+  final List<List<Position>> _shapeStates = [
+    [
+      Position(0, 0),
+      Position(0, 1),
+      Position(1, 0),
+      Position(1, -1),
+    ],
+  ]; // [0,0],[0,1],[1,0],[1,-1]
+}
+
+class ShapeJ extends Shape {
+  final List<List<Position>> _shapeStates = [
+    [
+      Position(0, 0),
+      Position(0, 1),
+      Position(1, 0),
+      Position(1, -1),
+    ],
+  ]; // [0,0],[0,1],[1,0],[1,-1]
+}
+
+class ShapeS extends Shape {
+  final List<List<Position>> _shapeStates = [
+    [
+      Position(0, 0),
+      Position(0, 1),
+      Position(1, 0),
+      Position(1, -1),
+    ],
+  ]; // [0,0],[0,1],[1,0],[1,-1]
+}
+
+class ShapeZ extends Shape {
+  final List<List<Position>> _shapeStates = [
+    [
+      Position(0, 0),
+      Position(0, 1),
+      Position(1, 0),
+      Position(1, -1),
+    ],
+  ]; // [0,0],[0,1],[1,0],[1,-1]
+}
+
+class ShapeT extends Shape {
+  final List<List<Position>> _shapeStates = [
+    [
+      Position(0, 0),
+      Position(0, 1),
+      Position(1, 0),
+      Position(1, -1),
+    ],
+  ]; // [0,0],[0,1],[1,0],[1,-1]
+}
+

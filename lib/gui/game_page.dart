561422c9e9fa31c8ed68ed464474ff552fd81379
Frank292 <f.merkel92@hotmail.com>
2021-03-31 12:16:33 +0200

    Update game_page.dart

--- lib/gui/game_page.dart
+++ lib/gui/game_page.dart
@@ -12,7 +12,6 @@ class GamePage extends HookWidget {
     final gameProvider = useProvider(providerGameProvider);
     final game = useProvider(providerGameProvider.state);
 
-
     return Material(
       child: Container(
         width: double.infinity,
@@ -108,17 +107,11 @@ class GamePage extends HookWidget {
           ],
         )),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: gameProvider.spawnShape,
-      ),
     );
   }
 
   List<Widget> _buildRows(BuildContext context, GameProvider gameProvider) {
     final list = <Widget>[];
-    final activeShapeX = gameProvider.getActiveShapePositionsX();
-    final activeShapeY = gameProvider.getActiveShapePositionsY();
-    int posInList;
     for (var y = Constant.numRows - 1; y >= 0; y--) {
       for (var x = 0; x < Constant.numCols; x++) {
         list.add(
@@ -127,23 +120,10 @@ class GamePage extends HookWidget {
                 border: const Border(top: BorderSide(), left: BorderSide()),
                 color: gameProvider.getInactiveShapeColor(x, y)),
             child: const Center(child: Text('X')),
-
           ),
         );
       }
     }
-    if (activeShapeY != null && activeShapeX != null)
-      for (var i = 0; i < activeShapeY.length; i++) {
-        posInList = activeShapeX[i] * Constant.numRows + activeShapeX[i];
-        list[posInList] = Container(
-          decoration: BoxDecoration(
-              border: const Border(top: BorderSide(), left: BorderSide()),
-              color: gameProvider.getActiveShapeColor(
-                  activeShapeX[i], activeShapeX[i])),
-          child: const Center(child: Text('X')),
-        );
-      }
-
     return list;
   }
 }

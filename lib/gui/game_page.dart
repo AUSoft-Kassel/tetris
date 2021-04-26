749657216e7650310226d956754b3c61945672f0
Andre <a.heinicke2988@gmail.com>
2021-04-26 14:27:00 +0200

    [CODE] Steuerkreuz links farbig anders Fixes #53 fertig?!

--- lib/gui/game_page.dart
+++ lib/gui/game_page.dart
@@ -43,8 +43,14 @@ class GamePage extends HookWidget {
                           color: Colors.amber,
                           child: Stack(
                             children: [
-                              ..._buildRows(context: context, gameProvider: gameProvider, blockSize: blockSize),
-                              ..._buildActiveShape(context: context, gameProvider: gameProvider, blockSize: blockSize),
+                              ..._buildRows(
+                                  context: context,
+                                  gameProvider: gameProvider,
+                                  blockSize: blockSize),
+                              ..._buildActiveShape(
+                                  context: context,
+                                  gameProvider: gameProvider,
+                                  blockSize: blockSize),
                             ],
                           ),
                         ),
@@ -71,7 +77,7 @@ class GamePage extends HookWidget {
                     ///Buttom
                     Container(
                         height: bottomSizeY,
-                        color: Colors.pink,
+                        color: Colors.black,
                         child: Flex(
                           direction: Axis.horizontal,
                           children: [
@@ -83,9 +89,18 @@ class GamePage extends HookWidget {
                                   //     color: Colors.blue,
                                   //   ),
                                   // ),
-                                  _buttonBox(alignment: Alignment(-0.8, -0.75), icon: Icons.keyboard_arrow_left_rounded, size: bottomSizeY / 2),
-                                  _buttonBox(alignment: Alignment(0.8, -0.75), icon: Icons.keyboard_arrow_right_rounded, size: bottomSizeY / 2),
-                                  _buttonBox(alignment: Alignment(0, 0.6), icon: Icons.keyboard_arrow_down_rounded, size: bottomSizeY / 2),
+                                  _buttonBox(
+                                      alignment: Alignment(-0.8, -0.75),
+                                      icon: Icons.keyboard_arrow_left_rounded,
+                                      size: bottomSizeY / 2),
+                                  _buttonBox(
+                                      alignment: Alignment(0.8, -0.75),
+                                      icon: Icons.keyboard_arrow_right_rounded,
+                                      size: bottomSizeY / 2),
+                                  _buttonBox(
+                                      alignment: Alignment(0, 0.6),
+                                      icon: Icons.keyboard_arrow_down_rounded,
+                                      size: bottomSizeY / 2),
                                 ],
                               ),
                             ),
@@ -97,8 +112,14 @@ class GamePage extends HookWidget {
                                   //     color: Colors.yellow,
                                   //   ),
                                   // ),
-                                  _buttonBox(alignment: Alignment(-0.75, 0.6), icon: Icons.rotate_left_rounded, size: bottomSizeY / 2.25),
-                                  _buttonBox(alignment: Alignment(0.75, -0.6), icon: Icons.rotate_right_rounded, size: bottomSizeY / 2.25),
+                                  _buttonBox(
+                                      alignment: Alignment(-0.75, 0.6),
+                                      icon: Icons.rotate_left_rounded,
+                                      size: bottomSizeY / 2.25),
+                                  _buttonBox(
+                                      alignment: Alignment(0.75, -0.6),
+                                      icon: Icons.rotate_right_rounded,
+                                      size: bottomSizeY / 2.25),
                                 ],
                               ),
                             ),
@@ -117,7 +138,10 @@ class GamePage extends HookWidget {
             })));
   }
 
-  List<Widget> _buildRows({required BuildContext context, required GameProvider gameProvider, required double blockSize}) {
+  List<Widget> _buildRows(
+      {required BuildContext context,
+      required GameProvider gameProvider,
+      required double blockSize}) {
     final list = <Widget>[];
     for (var y = Constant.numRows - 1; y >= 0; y--) {
       for (var x = 0; x < Constant.numCols; x++) {
@@ -140,7 +164,10 @@ class GamePage extends HookWidget {
     return list;
   }
 
-  List<Widget> _buildActiveShape({required BuildContext context, required GameProvider gameProvider, required double blockSize}) {
+  List<Widget> _buildActiveShape(
+      {required BuildContext context,
+      required GameProvider gameProvider,
+      required double blockSize}) {
     final list = <Widget>[];
     final positions = gameProvider.getActiveShapePositions();
     for (var pos in positions) {
@@ -162,14 +189,18 @@ class GamePage extends HookWidget {
     return list;
   }
 
-  Widget _buttonBox({required Alignment alignment, required IconData icon, required double size}) => Align(
+  Widget _buttonBox(
+          {required Alignment alignment,
+          required IconData icon,
+          required double size}) =>
+      Align(
         alignment: alignment,
         child: Container(
           width: size,
           height: size,
           child: FittedBox(
             fit: BoxFit.fill,
-            child: Icon(icon),
+            child: Icon(icon, color: Colors.orange),
           ),
         ),
       );

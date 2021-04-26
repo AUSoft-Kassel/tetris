88fecc9d320dcbb9990081c6fd1d605962207c0d
Valentin <valentinlaim@gmail.com>
2021-04-26 14:28:47 +0200

    [CODE] Start-Button schöner Fixes #51

--- lib/gui/game_page.dart
+++ lib/gui/game_page.dart
@@ -43,8 +43,14 @@ class GamePage extends HookWidget {
                           color: Colors.amber,
                           child: Stack(
                             children: [
-                              ..._buildRows(context: context, gameProvider: gameProvider, blockSize: blockSize),
-                              ..._buildActiveShape(context: context, gameProvider: gameProvider, blockSize: blockSize),
+                              ..._buildRows(
+                                  context: context,
+                                  gameProvider: gameProvider,
+                                  blockSize: blockSize),
+                              ..._buildActiveShape(
+                                  context: context,
+                                  gameProvider: gameProvider,
+                                  blockSize: blockSize),
                             ],
                           ),
                         ),
@@ -68,7 +74,7 @@ class GamePage extends HookWidget {
                       ],
                     ),
 
-                    ///Buttom
+                    ///Button
                     Container(
                         height: bottomSizeY,
                         color: Colors.pink,
@@ -83,9 +89,18 @@ class GamePage extends HookWidget {
                                   //     color: Colors.blue,
                                   //   ),
                                   // ),
-                                  _buttonBox(alignment: Alignment(-0.8, -0.75), icon: Icons.keyboard_arrow_left_rounded, size: bottomSizeY / 2),
-                                  _buttonBox(alignment: Alignment(0.8, -0.75), icon: Icons.keyboard_arrow_right_rounded, size: bottomSizeY / 2),
-                                  _buttonBox(alignment: Alignment(0, 0.6), icon: Icons.keyboard_arrow_down_rounded, size: bottomSizeY / 2),
+                                  _buttonBox(
+                                      alignment: Alignment(-0.8, -0.75),
+                                      icon: Icons.keyboard_arrow_left_rounded,
+                                      size: bottomSizeY / 2),
+                                  _buttonBox(
+                                      alignment: Alignment(0.8, -0.75),
+                                      icon: Icons.keyboard_arrow_right_rounded,
+                                      size: bottomSizeY / 2),
+                                  _buttonBox(
+                                      alignment: Alignment(0, 0.6),
+                                      icon: Icons.keyboard_arrow_down_rounded,
+                                      size: bottomSizeY / 2),
                                 ],
                               ),
                             ),
@@ -97,8 +112,14 @@ class GamePage extends HookWidget {
                                   //     color: Colors.yellow,
                                   //   ),
                                   // ),
-                                  _buttonBox(alignment: Alignment(-0.75, 0.6), icon: Icons.rotate_left_rounded, size: bottomSizeY / 2.25),
-                                  _buttonBox(alignment: Alignment(0.75, -0.6), icon: Icons.rotate_right_rounded, size: bottomSizeY / 2.25),
+                                  _buttonBox(
+                                      alignment: Alignment(-0.75, 0.6),
+                                      icon: Icons.rotate_left_rounded,
+                                      size: bottomSizeY / 2.25),
+                                  _buttonBox(
+                                      alignment: Alignment(0.75, -0.6),
+                                      icon: Icons.rotate_right_rounded,
+                                      size: bottomSizeY / 2.25),
                                 ],
                               ),
                             ),
@@ -109,15 +130,25 @@ class GamePage extends HookWidget {
                 if (!game.gameRunning)
                   Center(
                       child: TextButton(
-                          onPressed: () {
-                            gameProvider.startGame();
-                          },
-                          child: const Text('Start'))),
+                    onPressed: () {
+                      gameProvider.startGame();
+                    },
+                    child: Container(
+                        color: Colors.blue,
+                        child: const Text('Start Game',
+                            style: TextStyle(
+                              color: Colors.black87,
+                              backgroundColor: Colors.blue,
+                            ))),
+                  ))
               ]);
             })));
   }
 
-  List<Widget> _buildRows({required BuildContext context, required GameProvider gameProvider, required double blockSize}) {
+  List<Widget> _buildRows(
+      {required BuildContext context,
+      required GameProvider gameProvider,
+      required double blockSize}) {
     final list = <Widget>[];
     for (var y = Constant.numRows - 1; y >= 0; y--) {
       for (var x = 0; x < Constant.numCols; x++) {
@@ -140,7 +171,10 @@ class GamePage extends HookWidget {
     return list;
   }
 
-  List<Widget> _buildActiveShape({required BuildContext context, required GameProvider gameProvider, required double blockSize}) {
+  List<Widget> _buildActiveShape(
+      {required BuildContext context,
+      required GameProvider gameProvider,
+      required double blockSize}) {
     final list = <Widget>[];
     final positions = gameProvider.getActiveShapePositions();
     for (var pos in positions) {
@@ -162,7 +196,11 @@ class GamePage extends HookWidget {
     return list;
   }
 
-  Widget _buttonBox({required Alignment alignment, required IconData icon, required double size}) => Align(
+  Widget _buttonBox(
+          {required Alignment alignment,
+          required IconData icon,
+          required double size}) =>
+      Align(
         alignment: alignment,
         child: Container(
           width: size,

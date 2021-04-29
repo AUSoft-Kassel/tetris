3b2cc5cdd8709546fb26621b5c560fc93ab784d0
sbrandt893 <78741348+sbrandt893@users.noreply.github.com>
2021-04-29 11:09:22 +0200

    [CODE] Settings screen Fixes #69

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tetris/gui/game_page.dart';
+import 'package:tetris/gui/settings_page.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: GamePage(),
+      home: SettingsPage(),
     );
   }
 }

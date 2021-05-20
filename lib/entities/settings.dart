b1ec18d3c911fba2e84457b2b7f6aba12b1eece5
Andre <a.heinicke2988@gmail.com>
2021-05-20 10:03:45 +0200

    fixes

--- lib/entities/settings.dart
+++ lib/entities/settings.dart
@@ -1,11 +1,8 @@
 /// Settings
 class Settings {
-  /// the level of game sounds
-  final String _gameSound;
-
   /// All sounds on or off
   final bool _soundOn;
-  
+
   /// The Volume of the Sound
   final int _soundVolume;
 
@@ -18,22 +15,29 @@ class Settings {
   ///change the Gamemusic
   final String _gameMusic;
 
-  Settings({String gameSound = 'levelOne', bool soundOn = true, int soundVolume = 100, String nickName = 'Nick', double gameDifficult = 1,
-     String gameMusic='musicOne'}):
-  _gameSound = gameSound,
-  _soundOn = soundOn,
-  _soundVolume = soundVolume,
-  _nickName = nickName,
-  _gameDifficult = gameDifficult,
-  _gameMusic = gameMusic;
-  
-  Settings copyWith({String? gameSound, bool? soundOn, int? soundVolume, String? nickName, double? gameDifficult, String? gameMusic}) => 
-    return Settings(
-      gameSound: gameSound ?? _gameSound,
-      soundOn:soundOn ?? _soundOn,
-      soundVolume:soundVolume ?? _soundVolume, 
-      nickName:nickName ?? _nickName, 
-      gameDifficult:gameDifficult ?? _gameDifficult, 
-      gameMusic:gameMusic ?? _gameMusic
-    );
+  Settings(
+      {bool soundOn = true,
+      int soundVolume = 100,
+      String nickName = 'Nick',
+      double gameDifficult = 1,
+      String gameMusic = 'musicOne'})
+      : _soundOn = soundOn,
+        _soundVolume = soundVolume,
+        _nickName = nickName,
+        _gameDifficult = gameDifficult,
+        _gameMusic = gameMusic;
+
+  Settings copyWith(
+          {String? gameSound,
+          bool? soundOn,
+          int? soundVolume,
+          String? nickName,
+          double? gameDifficult,
+          String? gameMusic}) =>
+      Settings(
+          soundOn: soundOn ?? _soundOn,
+          soundVolume: soundVolume ?? _soundVolume,
+          nickName: nickName ?? _nickName,
+          gameDifficult: gameDifficult ?? _gameDifficult,
+          gameMusic: gameMusic ?? _gameMusic);
 }
